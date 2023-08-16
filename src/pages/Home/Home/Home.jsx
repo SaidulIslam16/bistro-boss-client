2755cdba7ab45a5694f07f3ca34eee3401c0adc0
SaidulIslam16 <isaidulislamsayed@gmail.com>
2023-08-16 16:49:17 +0600

    Carusel added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,10 @@
+import Banner from "../Banner/Banner";
 
 
 const Home = () => {
     return (
-        <div className="max-w-screen-xl border-2 mx-auto">
-            <h1>This is Home page</h1>
+        <div>
+            <Banner></Banner>
         </div>
     );
 };
