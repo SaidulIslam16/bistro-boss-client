d4fa8ab2ded27eea2801588926d35388eeed38cd
SaidulIslam16 <isaidulislamsayed@gmail.com>
2023-08-16 16:11:04 +0600

    Project created, Navbar and Footer added also router setup done

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div className="max-w-screen-xl border-2 mx-auto">
+            <h1>This is Home page</h1>
+        </div>
+    );
+};
+
+export default Home;
