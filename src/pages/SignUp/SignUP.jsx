3bae182464381edfb5750e12a7bd99c27334a775
SaidulIslam16 <isaidulislamsayed@gmail.com>
2023-08-18 21:33:04 +0600

    useing react hook form for signup

--- src/pages/SignUp/SignUP.jsx
+++ src/pages/SignUp/SignUP.jsx
@@ -1,31 +1,23 @@
-import { useContext, useEffect, useRef, useState } from 'react';
-import { loadCaptchaEnginge, LoadCanvasTemplate, LoadCanvasTemplateNoReload, validateCaptcha } from 'react-simple-captcha';
-import { AuthContext } from '../../providers/AuthProvider';
+import { useEffect, useRef, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import { loadCaptchaEnginge, LoadCanvasTemplate, LoadCanvasTemplateNoReload, validateCaptcha } from 'react-simple-captcha';
+
 
 const SignUP = () => {
 
     const captchaRef = useRef(null);
     const [disabled, setDisabled] = useState(true);
-    const { createUser } = useContext(AuthContext);
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const onSubmit = data => {
+        console.log(data)
+    };
+
 
     useEffect(() => {
         loadCaptchaEnginge(6);
     }, [])
-    const handleLogin = event => {
-        event.preventDefault();
-        const form = event.target;
-        const email = form.email.value;
-        const password = form.password.value;
-        createUser(email, password)
-            .then(result => {
-                const user = result.user;
-                console.log(user)
-            })
-            .catch(error => console.log(error.message))
-
-        // console.log(email, password)
-    }
 
     const handleValidateCaptcha = () => {
         const user_captcha_value = captchaRef.current.value;
@@ -39,6 +31,7 @@ const SignUP = () => {
             setDisabled(true)
         }
     }
+
     return (
         <div className="hero min-h-screen">
             <div className="hero-content md:flex">
@@ -47,24 +40,27 @@ const SignUP = () => {
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                 </div>
                 <div className="card w-full md:w-2/3 shadow-2xl bg-base-100">
-                    <form onSubmit={handleLogin} className="card-body">
+                    <form onSubmit={handleSubmit(onSubmit)} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Name</span>
                             </label>
-                            <input type="text" name="name" placeholder="Enter Name" className="input input-bordered" />
+                            <input type="text" {...register("name", { required: true })} name="name" placeholder="Enter Name" className="input input-bordered" />
+                            {errors.name && <span className='text-red-500'>Name is required</span>}
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" name="email" placeholder="Enter Email" className="input input-bordered" />
+                            <input type="email" {...register("email", { required: true })} name="email" placeholder="Enter Email" className="input input-bordered" />
+                            {errors.email && <span className='text-red-500'>Email is required</span>}
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" name="password" placeholder="Enter Password" className="input input-bordered" />
+                            <input type="password" {...register("password", { required: true, minLength: 6, maxLength: 20 })} name="password" placeholder="Enter Password" className="input input-bordered" />
+                            {errors.password?.type === 'required' && <p className='text-red-500'>Password is required</p>}
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
