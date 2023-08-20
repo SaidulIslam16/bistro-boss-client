79342a0cedc074dbd559e89fe3c9bf3b3c6d50b5
SaidulIslam16 <isaidulislamsayed@gmail.com>
2023-08-20 22:05:12 +0600

    showed products on my cart and implemented the delete method

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,9 +1,18 @@
-import { FaShoppingCart, FaWallet, FaCalendarAlt, FaHome, FaList } from "react-icons/fa";
+import { Helmet } from "react-helmet-async";
+import { FaShoppingCart, FaWallet, FaCalendarAlt, FaHome, FaList, FaEnvelope } from "react-icons/fa";
 import { Link, Outlet } from "react-router-dom";
+import useCart from "../hooks/useCart";
 
 const Dashboard = () => {
+
+    const [cart] = useCart();
+
     return (
+
         <div className="drawer lg:drawer-open">
+            <Helmet>
+                <title>Bistro Boss | DashBoard</title>
+            </Helmet>
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content flex flex-col items-center justify-center">
                 {/* Page content here */}
@@ -15,15 +24,15 @@ const Dashboard = () => {
                 <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
                 <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
                     {/* Sidebar content here */}
-                    <li><Link><FaHome /> Dashboard</Link></li>
-                    <li><Link to='/dashboard/mycart'><FaShoppingCart /> My Cart</Link></li>
-                    <li><Link><FaWallet /> Payment History</Link></li>
-                    <li><Link><FaCalendarAlt /> Reservation</Link></li>
+                    <li><Link to='/dashboard/home'><FaHome /> Dashboard</Link></li>
+                    <li><Link to='/dashboard/mycart'><FaShoppingCart /> My Cart <span className="badge badge-secondary">+{cart?.length || 0}</span></Link></li>
+                    <li><Link to='/dashboard/history'><FaWallet /> Payment History</Link></li>
+                    <li><Link to='/dashboard/reservation'><FaCalendarAlt /> Reservation</Link></li>
                     <div className="divider"></div>
                     <li><Link to='/'><FaHome /> Home</Link></li>
                     <li><Link to='/menu'><FaList /> Menu</Link></li>
-                    <li><Link to='/order/salad'><FaHome /> Order</Link></li>
-                    <li><Link to='/'><FaHome /> Home</Link></li>
+                    <li><Link to='/order/salad'><FaShoppingCart /> Order</Link></li>
+                    <li><Link to='/'><FaEnvelope /> Contact</Link></li>
 
                 </ul>
 
--- src/pages/Dashboard/MyCart/MyCart.jsx
+++ src/pages/Dashboard/MyCart/MyCart.jsx
@@ -1,8 +1,95 @@
+import { Helmet } from "react-helmet-async";
+import useCart from "../../../hooks/useCart";
+import { FaTrash } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const MyCart = () => {
+
+    const [cart, refetch] = useCart();
+    console.log(cart);
+    const total = cart.reduce((sum, item) => item.price + sum, 0);
+
+    const handdleDelete = item => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/cart/${item._id}`, {
+                    method: "DELETE"
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            refetch();
+                            Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                            )
+                        }
+                    })
+            }
+        })
+    }
+
     return (
-        <div>
-            <h2>My Cart Component</h2>
+        <div className="w-full">
+            <Helmet>
+                <title>Bistro Boss | My Cart</title>
+            </Helmet>
+            <div className="uppercase flex justify-between mb-6">
+                <h3 className="text-3xl">Total Items: {cart.length}</h3>
+                <h3 className="text-3xl">Total Price: ${total}</h3>
+                <button className="btn btn-warning">Pay Now</button>
+            </div>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead className="bg-gray-300">
+                        <tr>
+                            <th>#</th>
+                            <th>Item Image</th>
+                            <th>Item Name</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            cart.map((item, index) => <tr key={item._id}>
+                                <td>
+                                    {index + 1}
+                                </td>
+
+                                <td>
+                                    <div className="flex items-center space-x-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src={item.image} />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <p className="font-bold">{item.name}</p>
+                                </td>
+                                <td className="text-end">${item.price}</td>
+                                <th>
+                                    <button onClick={() => handdleDelete(item)} className="btn btn-ghost bg-red-600 text-white hover:bg-red-700"><FaTrash /></button>
+                                </th>
+                            </tr>)
+                        }
+
+                    </tbody>
+
+                </table>
+            </div>
         </div>
     );
 };
