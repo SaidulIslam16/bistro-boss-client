a6a89832743ba1bba6d746550cc2a333e2725f19
SaidulIslam16 <isaidulislamsayed@gmail.com>
2023-08-26 17:00:52 +0600

    publishable key added

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,10 +1,19 @@
+import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
+
+// TODO Provide Stripe api key
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
 
 const Payment = () => {
     return (
         <div className="w-full px-20">
             <SectionTitle subHeading='Please Process the' heading="Payment"></SectionTitle>
-
+            <h3>Onek Takar Bepar Separ</h3>
+            <Elements stripe={stripePromise}>
+                <CheckoutForm />
+            </Elements>
         </div>
     );
 };
